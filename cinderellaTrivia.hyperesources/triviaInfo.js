8cbf876d1fe192f1c1c4e36e8cf8d406e7cd6e78
Paulina Heng <120231847+paulinaheng@users.noreply.github.com>
2024-01-29 20:25:34 -0600

    Add files via upload

---
+++ cinderellaTrivia.hyperesources/triviaInfo.js
@@ -0,0 +1,79 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"Cinderella Trivia";
+	window.questions = [		"What is the names of Cinderella 2 mice friends?", 
+								"What is the name of Cinderella’s step mother?",
+								"During the scene in which Cinderella’s step sisters were ripping her dress, what color what the dress?", 
+								"What is the name of the first song that Cinderella sings?", 
+								"What is the name of Cinderella’s Step Sisters?",
+								"What was Cinderella’s first chore when she woke up in the morning?",
+								"Where did Cinderella first meet Gus?",
+								"What did Cinderella feed that chickens and mouse?",
+								"What is Gus’s real name?",
+								"Where did Cinderella meet her Fairy God Mother?" ];
+	window.correctAnswer = [ 	"D", "C", "A", "B", "E", "C", "D", "B", "A", "E"]; 
+	window.answerA = [ 			"Aaron and Nick", 
+								"Lady Katherine", 
+								"Pink", 
+								"So This is Love", 
+								"Ginger and Brigitte",
+								"Make Breakfast",
+								"Outside With the Chickens",
+								"Cheese",
+								"Octavius",
+								"In the Back Yard" ];
+	window.answerB = [ 			"Tom and Jerry", 
+								"Lady Dimitrescu", 
+								"Baby Blue", 
+								"A Dream is a Wish Your Heart Makes",
+								"Anne and Mary",
+								"Feed the Chickens",
+								"In the Kitchen Getting Chased by Lucifer",
+								"Corn",
+								"Gruff",
+								"The Front of the Mansion" ];
+	window.answerC = [ 			"Bing and Jeffy",
+								"Lady Tremaine",
+								"White", 
+								"Bibidi Bobidi Bo", 
+								"Elizabeth and Jane",
+								"Feed Lucifer",
+								"Under The Stairs",
+								"Fruits",
+								"Gillian",
+								"In Her Room" ];
+	window.answerD = [ 			"Jak and Gus",  
+								"Lady Goodall",
+								"Purple", 
+								"Sing Sweet Nightingale",
+								"Martha and Lucy",
+								"Wake Up her Step Mother",
+								"In a Mouse trap",
+								"Rice",
+								"Geoffry",
+								"In the Stables" ];
+	window.answerE = [ 			"Gruff and Loue", 
+								"Lady Montgomery", 
+								"Red", 
+								"Once Upon a Dream", 
+								"Anastasia and Drizella",
+								"Do Laundry",
+								"Under a Cup in the Pantry",
+								"Crackers",
+								"Lance",
+								"In the Garden" ];
+	window.response = [ 		"Jak and Gus are Cinderella best mouse friends. They helped her many time throughout the movie.", 
+								"Lady Tremaine got married to Cinderella's father after her mother died. She and her Daughters have been mean to Cinderella ever since her dad died.", 
+								"The dress used to be her mothers, it wa a little old fashioned so the mice helped her spice it up.", 
+								"In the beginning of the movie Cinderella is from a wonderful dreams and begins to sing the song.",  
+								"Anastasia and Drizella are Cinderella's step sisters. They both treat Cinderella like a servant and abuse her.",
+								"Cinderella is in charge of all of he household chores. Her first task is to feed Lucifer.",
+								"Whenever Cinderella finds s mouse she feeds and clothes them, which she did when she first finds Gus in the mouse trap.",
+								"One of her many chores is to feed the animals. She usually feeds them corn.",
+								"When Cinderella first finds Gus, she named him Octavius. But that name was a little long so she nicknamed him Gus.",
+								"After her step sisters rip apart her dress, she runs away into the garden and meets her fairy godmother for the first time."];
+							
+}
+						
